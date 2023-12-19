b475166dcce26ec1001ecabfc28b8d22710ffdea
Kevin Abishek S <Kevinabishek.S@microchip.com>
2023-12-19 09:22:00 +0530

    [UDP] Regenerated demo for udp_bootloader_bootloader_pic32cz_ca80_curiosity_ultra
    MH3-89214

--- apps/udp_bootloader/bootloader/firmware/src/config/pic32cz_ca80_curiosity_ultra/bootloader/bootloader_udp.c
+++ apps/udp_bootloader/bootloader/firmware/src/config/pic32cz_ca80_curiosity_ultra/bootloader/bootloader_udp.c
@@ -274,7 +274,7 @@ static void bootloader_ProcessBuffer( BOOTLOADER_DATA *handle )
 
         case (uint8_t)ERASE_FLASH:
         {
-bootloader_NvmAppErase(APP_START_ADDRESS, FLASH_END_ADDRESS);
+            bootloader_NvmAppErase(APP_START_ADDRESS, FLASH_END_ADDRESS);
             handle->currentState = BOOTLOADER_SEND_RESPONSE;
             handle->buffSize = 1;
             break;
