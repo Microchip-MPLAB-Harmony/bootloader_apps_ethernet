df2d4f23973ae01838d914e1e95176edbab91ce2
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-01-31 15:45:07 +0530

    Add ETHERNET bootloader demo for PIC32CZ CA70 Curiosity Ultra MH3-109939

---
+++ apps/udp_bootloader/bootloader/firmware/src/config/pic32cz_ca70_cult/bootloader/datastream/datastream_udp.c
@@ -0,0 +1,204 @@
+/*******************************************************************************
+ Data Stream UDP Source File
+
+  File Name:
+    datastream_udp.c
+
+  Summary:
+    Data Stream UDP source
+
+  Description:
+    This file contains source code necessary for the data stream interface.
+ *******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+//DOM-IGNORE-END
+
+#include "bootloader/datastream/datastream.h"
+#include "tcpip/tcpip.h"
+#include "definitions.h"
+
+#define BOOTLOADER_UDP_PORT_NUMBER      "6234"
+
+typedef struct
+{
+    DATASTREAM_HANDLE udpHandle;
+    eDIR currDir;
+    UDP_SOCKET udpSocket;
+    bool connected;
+    uint8_t *rxBuffer;
+    uint32_t rxMaxSize;
+    uint8_t *txBuffer;
+    uint32_t txMaxSize;
+    uintptr_t context;
+    DATASTREAM_HandlerType handler;
+} UDP_DATA;
+
+static UDP_DATA udpData;
+
+void DATASTREAM_Tasks(void)
+{
+
+    uint16_t avlBytes = 0;
+
+    if (udpData.connected == false)
+    {
+        return;
+    }
+
+    if (TX == udpData.currDir)
+    {
+        if(TCPIP_UDP_PutIsReady(udpData.udpSocket) >= udpData.txMaxSize)
+        {
+            (void) TCPIP_UDP_ArrayPut(udpData.udpSocket, udpData.txBuffer, (uint16_t)udpData.txMaxSize);
+            (void) TCPIP_UDP_Flush(udpData.udpSocket);
+            udpData.currDir = IDLE;
+            udpData.handler(DATASTREAM_BUFFER_EVENT_COMPLETE, (DATASTREAM_HANDLE)udpData.udpHandle, udpData.txMaxSize);
+
+        }
+    }
+
+    if (RX == udpData.currDir)
+    {
+        if (TCPIP_UDP_GetIsReady(udpData.udpSocket) != 0U)
+        {
+            avlBytes = TCPIP_UDP_GetIsReady(udpData.udpSocket);
+
+            avlBytes = TCPIP_UDP_ArrayGet(udpData.udpSocket, udpData.rxBuffer, avlBytes);
+            udpData.currDir = IDLE;
+            udpData.handler(DATASTREAM_BUFFER_EVENT_COMPLETE, (DATASTREAM_HANDLE)udpData.udpHandle, avlBytes);
+        }
+    }
+}
+
+/* MISRA C-2012 Rule 21.7 deviated:1 Deviation record ID -  H3_MISRAC_2012_R_21_7_DR_1 */
+
+DATASTREAM_HANDLE DATASTREAM_Open(const DRV_IO_INTENT ioIntent)
+{
+    SYS_STATUS          tcpipStat;
+    static IPV4_ADDR    dwLastIP[2] = { {0xFFFFFFFFU}, {0XFFFFFFFFU} };
+    IPV4_ADDR           ipAddr;
+    TCPIP_NET_HANDLE    netH;
+    int                 i, nNets;
+
+    udpData.udpHandle = DATASTREAM_HANDLE_INVALID;
+
+    tcpipStat = TCPIP_STACK_Status(sysObj.tcpip);
+
+    if(((uint32_t)tcpipStat < 0U) || (tcpipStat != SYS_STATUS_READY))
+    {
+        return (udpData.udpHandle);
+    }
+
+    // now that the stack is ready we can check the available interfaces
+    nNets = TCPIP_STACK_NumberOfNetworksGet();
+
+    for (i = 0; i < nNets; i++)
+    {
+        netH = TCPIP_STACK_IndexToNet(i);
+
+        ipAddr.Val = TCPIP_STACK_NetAddress(netH);
+
+        if(dwLastIP[i].Val != ipAddr.Val)
+        {
+            dwLastIP[i].Val = ipAddr.Val;
+
+            if (ipAddr.v[0] != 0U && ipAddr.v[0] != 169U) // Wait for a Valid IP
+            {
+                UDP_PORT port = (uint16_t)atoi(BOOTLOADER_UDP_PORT_NUMBER);
+
+                udpData.udpSocket = TCPIP_UDP_ServerOpen(IP_ADDRESS_TYPE_IPV4,
+                     port,
+                    (IP_MULTI_ADDRESS*) 0);
+
+                if (udpData.udpSocket == INVALID_SOCKET)
+                {
+                    return(DATASTREAM_HANDLE_INVALID);
+                }
+
+                udpData.connected = true;
+
+                udpData.udpHandle = 0;
+
+                break;
+            }
+        }
+    }
+
+    return(udpData.udpHandle);
+}
+
+/* MISRAC 2012 deviation block end */
+
+void DATASTREAM_Close(void)
+{
+    if (udpData.connected)
+    {
+        (void) TCPIP_UDP_Close(udpData.udpSocket);
+        TCPIP_STACK_Deinitialize(sysObj.tcpip);
+    }
+}
+
+DRV_CLIENT_STATUS DATASTREAM_ClientStatus(DATASTREAM_HANDLE handle)
+{
+    if(TCPIP_UDP_IsConnected(udpData.udpSocket) == true)
+    {
+        return DRV_CLIENT_STATUS_READY;
+    }
+
+    return DRV_CLIENT_STATUS_BUSY;
+}
+
+int DATASTREAM_Data_Read(DATASTREAM_HANDLE handle, uint8_t *buffer, const uint32_t rxSize)
+{
+    udpData.rxBuffer = buffer;
+    udpData.rxMaxSize = rxSize;
+    udpData.currDir = RX;
+    return(0);
+}
+
+int DATASTREAM_Data_Write(DATASTREAM_HANDLE handle, uint8_t *buffer, const uint32_t txSize)
+{
+    udpData.txBuffer = buffer;
+    udpData.txMaxSize = txSize;
+    udpData.currDir = TX;
+    return(0);
+}
+
+/* MISRA C-2012 Rule 11.1, 11.8 deviated below. Deviation record ID -  
+   H3_MISRAC_2012_R_11_1_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+
+void DATASTREAM_BufferEventHandlerSet
+(
+    const DATASTREAM_HANDLE hClient,
+    const void * eventHandler,
+    const uintptr_t context
+)
+{
+    udpData.handler = (DATASTREAM_HandlerType)eventHandler;
+    udpData.context = context;
+}
+
+/* MISRAC 2012 deviation block end */
---
+++ apps/udp_bootloader/bootloader/firmware/src/config/pic32cz_ca70_cult/initialization.c
@@ -0,0 +1,490 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2019 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+#pragma config TCM_CONFIGURATION = 0
+#pragma config SECURITY_BIT = CLEAR
+#pragma config BOOT_MODE = SET
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/* Following MISRA-C rules are deviated in the below code block */
+/* MISRA C-2012 Rule 7.2 */
+/* MISRA C-2012 Rule 11.1 */
+/* MISRA C-2012 Rule 11.3 */
+/* MISRA C-2012 Rule 11.8 */
+/* Forward declaration of GMAC initialization data */
+const TCPIP_MODULE_MAC_PIC32C_CONFIG tcpipGMACInitData;
+
+
+/* Forward declaration of MIIM 0 initialization data */
+static const DRV_MIIM_INIT drvMiimInitData_0;
+
+/* Forward declaration of PHY initialization data */
+const DRV_ETHPHY_INIT tcpipPhyInitData_LAN8720;
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+uint8_t txPrioNumToQueIndxGmac [DRV_GMAC_NUMBER_OF_QUEUES];
+uint8_t rxPrioNumToQueIndxGmac [DRV_GMAC_NUMBER_OF_QUEUES];
+
+/*** GMAC Initialization Data ***/
+TCPIP_MODULE_GMAC_QUEUE_CONFIG  gmac_queue_config[DRV_GMAC_NUMBER_OF_QUEUES]=
+{
+   {   /** QUEUE 0 Initialization**/
+       .queueTxEnable = true,
+       .queueRxEnable = true,
+       .nRxDescCnt    = 8,
+       .nTxDescCnt    = 8,
+       .rxBufferSize  = 1536,
+       .txMaxPktSize  = 1536,
+       .nRxDedicatedBuffers   = 8,
+       .nRxAddlBuffCount  = 2,
+       .nRxBuffCntThres   = 1,
+       .nRxBuffAllocCnt   = 2,   
+       .queueIntSrc       = GMAC_IRQn,                               
+   },
+   {   /** QUEUE 1 Initialization**/
+       .queueTxEnable = false,
+       .queueRxEnable = false,
+       .nRxDescCnt    = 1,
+       .nTxDescCnt    = 1,
+       .rxBufferSize  = 64,
+       .txMaxPktSize  = 464,
+       .nRxDedicatedBuffers   = 8,
+       .nRxAddlBuffCount  = 0,
+       .nRxBuffCntThres   = 1,
+       .nRxBuffAllocCnt   = 1,   
+       .queueIntSrc       = GMAC_Q1_IRQn,                               
+   },
+   {   /** QUEUE 2 Initialization**/
+       .queueTxEnable = false,
+       .queueRxEnable = false,
+       .nRxDescCnt    = 1,
+       .nTxDescCnt    = 1,
+       .rxBufferSize  = 64,
+       .txMaxPktSize  = 464,
+       .nRxDedicatedBuffers   = 1,
+       .nRxAddlBuffCount  = 0,
+       .nRxBuffCntThres   = 1,
+       .nRxBuffAllocCnt   = 1,   
+       .queueIntSrc       = GMAC_Q2_IRQn,                               
+   },
+   {   /** QUEUE 3 Initialization**/
+       .queueTxEnable = false,
+       .queueRxEnable = false,
+       .nRxDescCnt    = 1,
+       .nTxDescCnt    = 1,
+       .rxBufferSize  = 64,
+       .txMaxPktSize  = 1536,
+       .nRxDedicatedBuffers   = 1,
+       .nRxAddlBuffCount  = 0,
+       .nRxBuffCntThres   = 1,
+       .nRxBuffAllocCnt   = 1,   
+       .queueIntSrc       = GMAC_Q3_IRQn,                               
+   },
+   {   /** QUEUE 4 Initialization**/
+       .queueTxEnable = false,
+       .queueRxEnable = false,
+       .nRxDescCnt    = 1,
+       .nTxDescCnt    = 1,
+       .rxBufferSize  = 64,
+       .txMaxPktSize  = 1536,
+       .nRxDedicatedBuffers   = 1,
+       .nRxAddlBuffCount  = 0,
+       .nRxBuffCntThres   = 1,
+       .nRxBuffAllocCnt   = 1,   
+       .queueIntSrc       = GMAC_Q4_IRQn,                               
+   },
+   {   /** QUEUE 5 Initialization**/
+       .queueTxEnable = false,
+       .queueRxEnable = false,
+       .nRxDescCnt    = 1,
+       .nTxDescCnt    = 1,
+       .rxBufferSize  = 64,
+       .txMaxPktSize  = 976,
+       .nRxDedicatedBuffers   = 1,
+       .nRxAddlBuffCount  = 0,
+       .nRxBuffCntThres   = 1,
+       .nRxBuffAllocCnt   = 1,   
+       .queueIntSrc       = GMAC_Q5_IRQn,                               
+   },
+};
+
+const TCPIP_MODULE_MAC_PIC32C_CONFIG tcpipGMACInitData =
+{ 
+       .gmac_queue_config = gmac_queue_config,
+       .macQueNum = DRV_GMAC_NUMBER_OF_QUEUES, 
+       .txPrioNumToQueIndx = txPrioNumToQueIndxGmac,
+       .rxPrioNumToQueIndx = rxPrioNumToQueIndxGmac,
+       .ethFlags               = TCPIP_GMAC_ETH_OPEN_FLAGS,    
+       .linkInitDelay          = DRV_LAN8720_PHY_LINK_INIT_DELAY,
+       .ethModuleId            = TCPIP_GMAC_MODULE_ID,
+       .pPhyBase               = &DRV_ETHPHY_OBJECT_BASE_Default,
+       .pPhyInit               = &tcpipPhyInitData_LAN8720,
+       .checksumOffloadRx      = DRV_GMAC_RX_CHKSM_OFFLOAD,
+       .checksumOffloadTx      = DRV_GMAC_TX_CHKSM_OFFLOAD,
+       .macTxPrioNum           = TCPIP_GMAC_TX_PRIO_COUNT,
+       .macRxPrioNum           = TCPIP_GMAC_RX_PRIO_COUNT,  
+       .macRxFilt              = TCPIP_GMAC_RX_FILTERS,
+};
+
+
+/*** MIIM Driver Instance 0 Configuration ***/
+static const DRV_MIIM_INIT drvMiimInitData_0 =
+{
+   .miimId = DRV_MIIM_ETH_MODULE_ID_0,
+};
+
+/*** LAN8720 PHY Driver Time-Out Initialization Data ***/
+DRV_ETHPHY_TMO drvlan8720Tmo = 
+{
+    .resetTmo = DRV_ETHPHY_LAN8720_RESET_CLR_TMO,
+    .aNegDoneTmo = DRV_ETHPHY_LAN8720_NEG_DONE_TMO,
+    .aNegInitTmo = DRV_ETHPHY_LAN8720_NEG_INIT_TMO,    
+};
+
+/*** ETH PHY Initialization Data ***/
+const DRV_ETHPHY_INIT tcpipPhyInitData_LAN8720 =
+{    
+    .ethphyId               = DRV_LAN8720_PHY_PERIPHERAL_ID,
+    .phyAddress             = DRV_LAN8720_PHY_ADDRESS,
+    .phyFlags               = DRV_LAN8720_PHY_CONFIG_FLAGS,
+    .pPhyObject             = &DRV_ETHPHY_OBJECT_LAN8720,
+    .ethphyTmo              = &drvlan8720Tmo,
+    .pMiimObject            = &DRV_MIIM_OBJECT_BASE_Default,
+    .pMiimInit              = &drvMiimInitData_0,
+    .miimIndex              = 0,
+
+
+    .resetFunction          = 0,
+};
+
+
+
+// <editor-fold defaultstate="collapsed" desc="TCP/IP Stack Initialization Data">
+// *****************************************************************************
+// *****************************************************************************
+// Section: TCPIP Data
+// *****************************************************************************
+// *****************************************************************************
+/*** ARP Service Initialization Data ***/
+const TCPIP_ARP_MODULE_CONFIG tcpipARPInitData =
+{ 
+    .cacheEntries       = TCPIP_ARP_CACHE_ENTRIES,     
+    .deleteOld          = TCPIP_ARP_CACHE_DELETE_OLD,    
+    .entrySolvedTmo     = TCPIP_ARP_CACHE_SOLVED_ENTRY_TMO, 
+    .entryPendingTmo    = TCPIP_ARP_CACHE_PENDING_ENTRY_TMO, 
+    .entryRetryTmo      = TCPIP_ARP_CACHE_PENDING_RETRY_TMO, 
+    .permQuota          = TCPIP_ARP_CACHE_PERMANENT_QUOTA, 
+    .purgeThres         = TCPIP_ARP_CACHE_PURGE_THRESHOLD, 
+    .purgeQuanta        = TCPIP_ARP_CACHE_PURGE_QUANTA, 
+    .retries            = TCPIP_ARP_CACHE_ENTRY_RETRIES, 
+    .gratProbeCount     = TCPIP_ARP_GRATUITOUS_PROBE_COUNT,
+};
+
+
+/*** UDP Sockets Initialization Data ***/
+const TCPIP_UDP_MODULE_CONFIG tcpipUDPInitData =
+{
+    .nSockets       = TCPIP_UDP_MAX_SOCKETS,
+    .sktTxBuffSize  = TCPIP_UDP_SOCKET_DEFAULT_TX_SIZE, 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*** IPv4 Initialization Data ***/
+
+
+const TCPIP_IPV4_MODULE_CONFIG  tcpipIPv4InitData = 
+{
+    .arpEntries = TCPIP_IPV4_ARP_SLOTS, 
+};
+
+
+
+
+
+
+TCPIP_STACK_HEAP_INTERNAL_CONFIG tcpipHeapConfig =
+{
+    .heapType = TCPIP_STACK_HEAP_TYPE_INTERNAL_HEAP,
+    .heapFlags = TCPIP_STACK_HEAP_USE_FLAGS,
+    .heapUsage = TCPIP_STACK_HEAP_USAGE_CONFIG,
+    .malloc_fnc = TCPIP_STACK_MALLOC_FUNC,
+    .free_fnc = TCPIP_STACK_FREE_FUNC,
+    .heapSize = TCPIP_STACK_DRAM_SIZE,
+};
+
+
+const TCPIP_NETWORK_CONFIG __attribute__((unused))  TCPIP_HOSTS_CONFIGURATION[] =
+{
+    /*** Network Configuration Index 0 ***/
+    {
+        .interface = TCPIP_NETWORK_DEFAULT_INTERFACE_NAME_IDX0,
+        .hostName = TCPIP_NETWORK_DEFAULT_HOST_NAME_IDX0,
+        .macAddr = TCPIP_NETWORK_DEFAULT_MAC_ADDR_IDX0,
+        .ipAddr = TCPIP_NETWORK_DEFAULT_IP_ADDRESS_IDX0,
+        .ipMask = TCPIP_NETWORK_DEFAULT_IP_MASK_IDX0,
+        .gateway = TCPIP_NETWORK_DEFAULT_GATEWAY_IDX0,
+        .priDNS = TCPIP_NETWORK_DEFAULT_DNS_IDX0,
+        .secondDNS = TCPIP_NETWORK_DEFAULT_SECOND_DNS_IDX0,
+        .powerMode = TCPIP_NETWORK_DEFAULT_POWER_MODE_IDX0,
+        .startFlags = TCPIP_NETWORK_DEFAULT_INTERFACE_FLAGS_IDX0,
+        .pMacObject = &TCPIP_NETWORK_DEFAULT_MAC_DRIVER_IDX0,
+    },
+};
+
+const size_t TCPIP_HOSTS_CONFIGURATION_SIZE = sizeof (TCPIP_HOSTS_CONFIGURATION) / sizeof (*TCPIP_HOSTS_CONFIGURATION);
+
+const TCPIP_STACK_MODULE_CONFIG TCPIP_STACK_MODULE_CONFIG_TBL [] =
+{
+    {TCPIP_MODULE_IPV4,             &tcpipIPv4InitData},
+
+
+    {TCPIP_MODULE_ARP,              &tcpipARPInitData},             // TCPIP_MODULE_ARP
+    {TCPIP_MODULE_UDP,              &tcpipUDPInitData},             // TCPIP_MODULE_UDP
+
+    { TCPIP_MODULE_MANAGER,         &tcpipHeapConfig },             // TCPIP_MODULE_MANAGER
+
+// MAC modules
+    {TCPIP_MODULE_MAC_PIC32C,       &tcpipGMACInitData},            // TCPIP_MODULE_MAC_PIC32C
+
+};
+
+const size_t TCPIP_STACK_MODULE_CONFIG_TBL_SIZE = sizeof (TCPIP_STACK_MODULE_CONFIG_TBL) / sizeof (*TCPIP_STACK_MODULE_CONFIG_TBL);
+/*********************************************************************
+ * Function:        SYS_MODULE_OBJ TCPIP_STACK_Init()
+ *
+ * PreCondition:    None
+ *
+ * Input:
+ *
+ * Output:          valid system module object if Stack and its componets are initialized
+ *                  SYS_MODULE_OBJ_INVALID otherwise
+ *
+ * Overview:        The function starts the initialization of the stack.
+ *                  If an error occurs, the SYS_ERROR() is called
+ *                  and the function de-initialize itself and will return false.
+ *
+ * Side Effects:    None
+ *
+ * Note:            This function must be called before any of the
+ *                  stack or its component routines are used.
+ *
+ ********************************************************************/
+
+
+SYS_MODULE_OBJ TCPIP_STACK_Init(void)
+{
+    TCPIP_STACK_INIT    tcpipInit;
+
+    tcpipInit.pNetConf = TCPIP_HOSTS_CONFIGURATION;
+    tcpipInit.nNets = TCPIP_HOSTS_CONFIGURATION_SIZE;
+    tcpipInit.pModConfig = TCPIP_STACK_MODULE_CONFIG_TBL;
+    tcpipInit.nModules = TCPIP_STACK_MODULE_CONFIG_TBL_SIZE;
+    tcpipInit.initCback = 0;
+
+    return TCPIP_STACK_Initialize(0, &tcpipInit.moduleInit);
+}
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="SYS_TIME Initialization Data">
+
+static const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
+    .timerCallbackSet = (SYS_TIME_PLIB_CALLBACK_REGISTER)TC0_CH0_TimerCallbackRegister,
+    .timerStart = (SYS_TIME_PLIB_START)TC0_CH0_TimerStart,
+    .timerStop = (SYS_TIME_PLIB_STOP)TC0_CH0_TimerStop ,
+    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)TC0_CH0_TimerFrequencyGet,
+    .timerPeriodSet = (SYS_TIME_PLIB_PERIOD_SET)TC0_CH0_TimerPeriodSet,
+    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)TC0_CH0_TimerCompareSet,
+    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)TC0_CH0_TimerCounterGet,
+};
+
+static const SYS_TIME_INIT sysTimeInitData =
+{
+    .timePlib = &sysTimePlibAPI,
+    .hwTimerIntNum = TC0_CH0_IRQn,
+};
+
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* MISRAC 2012 deviation block end */
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+*/
+
+void SYS_Initialize ( void* data )
+{
+    /* MISRAC 2012 deviation block start */
+    /* MISRA C-2012 Rule 2.2 deviated in this file.  Deviation record ID -  H3_MISRAC_2012_R_2_2_DR_1 */
+    EFC_Initialize();
+
+    CLOCK_Initialize();
+    
+    PIO_Initialize();
+
+    if (bootloader_Trigger() == false)
+    {
+        run_Application(APP_START_ADDRESS);
+    }
+
+
+
+	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
+
+	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
+
+	BSP_Initialize();
+  
+
+ 
+    TC0_CH0_TimerInitialize(); 
+     
+    
+
+    /* MISRAC 2012 deviation block start */
+    /* Following MISRA-C rules deviated in this block  */
+    /* MISRA C-2012 Rule 11.3 - Deviation record ID - H3_MISRAC_2012_R_11_3_DR_1 */
+    /* MISRA C-2012 Rule 11.8 - Deviation record ID - H3_MISRAC_2012_R_11_8_DR_1 */
+
+   /* Initialize the MIIM Driver Instance 0*/
+   sysObj.drvMiim_0 = DRV_MIIM_OBJECT_BASE_Default.DRV_MIIM_Initialize(DRV_MIIM_DRIVER_INDEX_0, (const SYS_MODULE_INIT *) &drvMiimInitData_0); 
+
+
+    /* MISRA C-2012 Rule 11.3, 11.8 deviated below. Deviation record ID -  
+    H3_MISRAC_2012_R_11_3_DR_1 & H3_MISRAC_2012_R_11_8_DR_1*/
+        
+    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+    
+    /* MISRAC 2012 deviation block end */
+
+
+   /* TCPIP Stack Initialization */
+   sysObj.tcpip = TCPIP_STACK_Init();
+   SYS_ASSERT(sysObj.tcpip != SYS_MODULE_OBJ_INVALID, "TCPIP_STACK_Init Failed" );
+
+
+
+    /* MISRAC 2012 deviation block end */
+    APP_Initialize();
+
+
+    NVIC_Initialize();
+
+    /* MISRAC 2012 deviation block end */
+}
