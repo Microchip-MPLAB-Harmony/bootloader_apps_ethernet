ae876858c550250f5c087f4f4863fe77fe238acd
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-05-14 15:28:20 +0530

    [APPS] Regenerated Demo for pic32cz_ca70_cult
    MH3-112241

--- apps/udp_bootloader/test_app/firmware/src/config/pic32cz_ca70_cult/user.h
+++ apps/udp_bootloader/test_app/firmware/src/config/pic32cz_ca70_cult/user.h
@@ -19,6 +19,8 @@
 #ifndef USER_H
 #define USER_H
 
+#include "bsp/bsp.h"
+
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
 
@@ -32,7 +34,22 @@ extern "C" {
 // Section: User Configuration macros
 // *****************************************************************************
 // *****************************************************************************
+#define LED_ON()        LED0_On()
+#define LED_OFF()       LED0_Off()
+#define LED_TOGGLE()    LED0_Toggle()
 
+#define SWITCH_GET()    SW0_Get()
+#define SWITCH_PRESSED  SW0_STATE_PRESSED
+    
+#define APP_TIMER_START     SYSTICK_TimerStart
+#define APP_TIMER_DelayMs   SYSTICK_DelayMs
+
+#define BTL_TRIGGER_RAM_START 0x20400000
+
+static inline void APP_SystemReset( void )
+{
+    NVIC_SystemReset();
+}
 
 //DOM-IGNORE-BEGIN
 #ifdef __cplusplus
